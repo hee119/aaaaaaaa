467d2279920d008992e1369f8c3dac36c8415719
hongyeseong <eunpyohong44@gmail.com>
2026-01-12 13:47:23 +0900

    a

--- DiceAttack/Assets/DiceGame/script/Dice.cs
+++ DiceAttack/Assets/DiceGame/script/Dice.cs
@@ -21,17 +21,17 @@ public class Dice : MonoBehaviour
                 // 1. 색상 값을 변경하고
                 _renderer.color = Color.blue;
             }
-            else if (gameObject.CompareTag("GambleDice"))
-            {
-                gameObject.tag = "AttackDice";
-                // 2. 즉시 적용합니다.
-                _renderer.color = Color.red;
-            }
             else if (gameObject.CompareTag("DefenceDice"))
             {
                 gameObject.tag = "GambleDice";
                 // 2. 즉시 적용합니다.
                 _renderer.color = Color.white;
+            }
+            else if (gameObject.CompareTag("GambleDice"))
+            {
+                gameObject.tag = "AttackDice";
+                // 2. 즉시 적용합니다.
+                _renderer.color = Color.red;
             }
         }
         if (NewMonoBehaviourScript.Instance != null && NewMonoBehaviourScript.Instance.win)
@@ -61,7 +61,7 @@ public class Dice : MonoBehaviour
                 _renderer.color = Color.red;
             }
         }
-        else
+        if(NewMonoBehaviourScript.Instance != null && (!NewMonoBehaviourScript.Instance.win || !GambleGame.Instance.win))
         {
             if (gameObject.CompareTag("AttackDice"))
             {
