ad968a770b5776001a2d23f63988ce0c263cd0dd
Blynx5064 <karis75455@gmail.com>
2026-01-13 17:14:17 +0900

    크아악 시이@ 바로 UI 정상화! 바로 정상화 들어가는 임창섭~

---
+++ DiceAttack/Assets/DiceGame/script/OpenStatScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenStatScene : MonoBehaviour
+{
+    public void OpenScene()
+
+    {
+
+        SceneManager.LoadScene("selectStat");
+
+    }
+}
